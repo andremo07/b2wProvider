4a528e36819eb5bf192c9a2a86967250a7aacd1a
Unknown <ext-andre.oliveira@imusica.com.br>
2018-08-29 17:08:23 -0300

    adequando a atualização do restProvider

--- src/main/java/br/com/trendsoftware/b2wprovider/service/ShippingService.java
+++ src/main/java/br/com/trendsoftware/b2wprovider/service/ShippingService.java
@@ -1,20 +1,23 @@
 package br.com.trendsoftware.b2wprovider.service;
 
-import com.ning.http.client.FluentCaseInsensitiveStringsMap;
-import com.ning.http.client.FluentStringsMap;
-import com.ning.http.client.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.asynchttpclient.Response;
+
+import br.com.trendsoftware.b2wprovider.http.client.B2wClient;
 import br.com.trendsoftware.restProvider.exception.MessageException;
 import br.com.trendsoftware.restProvider.exception.RestClientException;
 import br.com.trendsoftware.restProvider.exception.ServiceException;
 
 public class ShippingService extends B2wService{
 		
-	public Response getShippingById(FluentCaseInsensitiveStringsMap headers, String shippingId)throws ServiceException{
+	public Response getShippingById(Map<String,String> headers, String shippingId)throws ServiceException{
 
 		try {
-			FluentStringsMap params = new FluentStringsMap();
-			Response response = b2w.get("/shipments/"+shippingId,params,headers);
+			Map<String, List<String>> params = new  HashMap<String, List<String>>();
+			Response response = B2wClient.get(B2wClient.API_URL,"/shipments/"+shippingId,params,headers);
 			return response;	
 		} catch (RestClientException e) {	
 			throw new ServiceException(String.format("%s:%s", MessageException.ERROR_QUERY_SHIPPING_INFO, e.getMessage()), e);
--- src/main/java/br/com/trendsoftware/b2wprovider/service/UserService.java
+++ src/main/java/br/com/trendsoftware/b2wprovider/service/UserService.java
@@ -1,26 +1,27 @@
 package br.com.trendsoftware.b2wprovider.service;
 
-import com.ning.http.client.FluentCaseInsensitiveStringsMap;
-import com.ning.http.client.FluentStringsMap;
-import com.ning.http.client.Response;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.asynchttpclient.Response;
+
+import br.com.trendsoftware.b2wprovider.http.client.B2wClient;
 import br.com.trendsoftware.restProvider.exception.MessageException;
 import br.com.trendsoftware.restProvider.exception.RestClientException;
 import br.com.trendsoftware.restProvider.exception.ServiceException;
 
 public class UserService extends B2wService{
 			
-	public Response getUserInfo(FluentCaseInsensitiveStringsMap headers)throws ServiceException{
+	public Response getUserInfo(Map<String,String> headers)throws ServiceException{
 
 		try {
-			FluentStringsMap params = new FluentStringsMap();
-			Response response = b2w.get("/users/me",params,headers);
+			Map<String, List<String>> params = new  HashMap<String, List<String>>();
+			Response response = B2wClient.get(B2wClient.API_URL,"/users/me",params,headers);
 			return response;	
 		} catch (RestClientException e) {	
 			throw new ServiceException(String.format("%s:%s", MessageException.ERROR_QUERY_USER_INFO, e.getMessage()), e);
 		}
 		
 	}
-	
-
 }
